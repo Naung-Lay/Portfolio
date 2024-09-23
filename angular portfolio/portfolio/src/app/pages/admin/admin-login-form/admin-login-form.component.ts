7d51f4c451a84eedd78eb114a1046c9aacb3de18
Khant Win Soe <131604026+Naung-Lay@users.noreply.github.com>
2024-09-23 20:39:55 +0630

    remove angular project

--- angular portfolio/portfolio/src/app/pages/admin/admin-login-form/admin-login-form.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-admin-login-form',
-  templateUrl: './admin-login-form.component.html',
-  styleUrl: './admin-login-form.component.css',
-})
-export class AdminLoginFormComponent {}
